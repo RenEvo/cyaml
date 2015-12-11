ca2bfd5d448bb358777f35c8f1570c34f5204cd0
Tom Anderson <tanderson@blizzard.com>
2015-12-10 22:53:41 -0800

    Bootstrapping unit tests.

---
+++ test/basic.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+
+describe('Compressed :Yaml', function() {
+    describe('Simple Read', function() {
+        it('Should load basic things', function(done) {
+
+
+            done();
+        });
+    });
+});
